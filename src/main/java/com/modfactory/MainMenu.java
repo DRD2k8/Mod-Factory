44b92ce2d299629dbaef5b2cdd538e5869a125f7
Dandi2k8 <drd.construction30@gmail.com>
2024-06-02 11:21:25 -0700

    Add files

--- src/main/java/com/modfactory/MainMenu.java
+++ src/main/java/com/modfactory/MainMenu.java
@@ -11,7 +11,7 @@ public class MainMenu extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainMenu.class.getResource("main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/modfactory/plugin/Plugin.java
@@ -0,0 +1,4 @@
+package com.modfactory.plugin;
+
+public class Plugin {
+}
